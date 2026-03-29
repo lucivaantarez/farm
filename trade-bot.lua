60536c114241ea472f1391e12e68aa11e0384dcd
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-03-29 16:02:21 +0700

    Create trade-bot.lua

---
+++ trade-bot.lua
@@ -0,0 +1,16 @@
+script_key="aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
+getgenv().AutoTradeConfig = {
+    MainAccount = "mimabau1904",
+    Items = {
+        "Race Reroll",
+        "Clan Reroll",
+        "Mythical Chest",
+        "Aura Crate",
+        "Cosmetic Crate",
+        "Trait Reroll",
+        "Abyss Sigil",
+        "Broken Sword",
+        "Passive Shard",
+    },
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/75c7fe88bf77410a404199a69629aae3.lua"))()
