0392cb19726557ec763a7159010c3f4fed01f38e
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-03-29 16:53:47 +0700

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -1,13 +1,6 @@
---[[
-    Auto Trade (Gom Item) - Sailor Piece
-    Chạy trên CẢ 2 acc: clone gửi trade + add item, main tự accept + ready + confirm
-]]
 script_key="aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
 getgenv().AutoTradeConfig = {
-    -- Tên acc main
     MainAccount = "mimabau1904",
-
-    -- Danh sách tên item muốn trade 
     Items = {
         "Race Reroll",
         "Clan Reroll",
