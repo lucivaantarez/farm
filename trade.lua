cb7a7d680b1bb0266ffa0f5d559b3a5db0a233ba
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-02 05:12:19 +0700

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -35,8 +35,7 @@ getgenv().AutoTradeConfig = {
         "Atomic Core",
         "Azure Heart",
         "Broken Sword",
-        "Tempest Relic"
-    }
+        "Tempest Relic",
+    },
 }
-
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/75c7fe88bf77410a404199a69629aae3.lua"))()
