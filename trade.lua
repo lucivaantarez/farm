1d1fa9b159cf4dfa7f5d26fd81ecc70d092e4c1e
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-02 05:02:33 +0700

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -1,17 +1,42 @@
-script_key="aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
+_G.Script_key = "aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
 getgenv().AutoTradeConfig = {
     MainAccount = "mimabau1904",
     Items = {
+        -- [ CATEGORY: REROLLS ] --
         "Race Reroll",
         "Clan Reroll",
         "Trait Reroll",
+        -- [ CATEGORY: CHESTS & CRATES ] --
         "Mythical Chest",
         "Secret Chest",
         "Aura Crate",
         "Cosmetic Crate",
-        "Abyss Sigil",
-        "Broken Sword",
+        -- [ CATEGORY: KEYS & TICKETS ] --
+        "Dungeon Ticket",
+        "Boss Ticket",
+        "Boss Rush Ticket",
+        "Dungeon Key",
+        "Tower Key",
+        "Boss Key",
+        "Slime Key",
+        "Malevolent Key",
+        "Divine Grail",
+        "Dark Grail",
+        -- [ CATEGORY: SHARDS & SPECIAL MATERIALS ] --
         "Passive Shard",
-    },
+        "Power Shard",
+        "Abyss Sigil",
+        "Chrysalis Sigil",
+        "Upper Seal",
+        "Calamity Seal",
+        "Battle Sigil",
+        "Cursed Finger",
+        "Hogyoku Frag",
+        "Atomic Core",
+        "Azure Heart",
+        "Broken Sword",
+        "Tempest Relic"
+    }
 }
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/75c7fe88bf77410a404199a69629aae3.lua"))()
