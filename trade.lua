2ae74d8609487a270240c1e2e328326e58630f01
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-03 00:58:53 +0700

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -2,61 +2,119 @@ script_key="aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
 getgenv().AutoTradeConfig = {
     MainAccount = "mimabau1904",
     Items = {
-        -- [ CATEGORY: REROLLS ] --
+        -- [[ UTILITY & LOOT ]] --
         "Race Reroll",
         "Clan Reroll",
         "Trait Reroll",
-        "Haki Color Reroll",        
-        -- [ CATEGORY: CHESTS & CRATES ] --
+        "Haki Color Reroll",
         "Mythical Chest",
         "Secret Chest",
         "Aura Crate",
         "Cosmetic Crate",
-        -- [ CATEGORY: KEYS & TICKETS ] --
-        "Dungeon Ticket",
-        "Boss Ticket",
-        "Boss Rush Ticket",
-        "Dungeon Key",
-        "Tower Key",
-        "Boss Key",
-        "Slime Key",
-        "Malevolent Key",
-        "Divine Grail",
-        "Dark Grail",
-        -- [ CATEGORY: SHARDS & SPECIAL MATERIALS ] --
-        "Passive Shard",
-        "Power Shard",
-        "Abyss Sigil",
-        "Chrysalis Sigil",
-        "Upper Seal",
-        "Calamity Seal",
-        "Battle Sigil",
-        "Cursed Finger",
-        "Hogyoku Frag",
-        "Atomic Core",
-        "Azure Heart",
-        "Broken Sword",
-        "Tempest Relic",
+
+        -- [[ SET: GOJO V2 (Strongest of Today) ]] --
+        "Limitless Key",
         "Infinity Essence",
         "Blue Singularity",
         "Reversal Pulse",
         "Energy Shard",
         "Cursed Flesh",
+
+        -- [[ SET: GILGAMESH (King of Heroes) ]] --
+        "Divine Grail",
         "Throne Remnant",
         "Ancient Shard",
         "Golden Essence",
         "Phantasm Core",
+        "Broken Sword",
+
+        -- [[ SET: YAMATO (Blade Sovereign) ]] --
+        "Azure Heart",
         "Silent Storm",
         "Yamato Essence",
         "Frozen Will",
-        "Worthiness Fragment",
+
+        -- [[ SET: TRUE AIZEN / AIZEN ]] --
+        "Chrysalis Sigil",
         "Evolution Fragment",
         "Transcendent Core",
+        "Fusion Ring",
+        "Worthiness Fragment",
+        "Hogyoku Frag",
+        "Mirage Pendant",
+        "Illusion Prism",
+        "Reiatsu Core",
+        "Boss Ticket",
+
+        -- [[ SET: KOKUSHIBO (Moon Slayer) ]] --
+        "Upper Seal",
+        "Moon Crest",
+        "Crescent Shard",
+        "Lunar Essence",
+        "Demon Remnant",
+
+        -- [[ SET: ATOMIC (Cid V2 / Shadow) ]] --
+        "Abyss Sigil",
+        "Atomic Core",
+        "Atomic Omen",
+        "Eminence Essence",
+        "Shadow Remnant",
+        "Magic Shard",
+        "Void Seed",
+        "Umbral Capsule",
+
+        -- [[ SET: SHADOW MONARCH (SJW V2 / Solo Hunter) ]] --
+        "Dungeon Ticket",
+        "Monarch Core",
+        "Monarch Essence",
+        "Kamish Dagger",
+        "Shadow Crystal",
+        "Abyss Edge",
+        "Dark Ring",
+        "Shadow Heart",
+
+        -- [[ SET: SABER ALTER (Corrupted Excalibur) ]] --
+        "Dark Grail",
+        "Corrupt Crown",
+        "Corruption Core",
+        "Alter Essence",
+        "Morgan Remnant",
+
+        -- [[ SET: SUKUNA V1/V2 ]] --
+        "Malevolent Key",
+        "Cursed Finger",
+        "Dismantle Fang",
+        "Crimson Heart",
+        "Vessel Ring",
+        "Malevolent Soul",
+        "Awakened Cursed Finger",
+
+        -- [[ SET: MADARA (Strongest Shinobi) ]] --
+        "Battle Sigil",
+        "Path Fragment",
+        "Eternal Core",
+        "Power Remnant",
+
+        -- [[ SET: RIMURU ]] --
+        "Slime Key",
+        "Tempest Relic",
+        "Sage Pulse",
+        "Tempest Seal",
+        "Slime Remnant",
+        "Slime Core",
+
+        -- [[ SET: ANOS / MISC ]] --
+        "Calamity Seal",
+        "Boss Rush Ticket",
+        "Dungeon Key",
+        "Tower Key",
+        "Boss Key",
+        "Passive Shard",
+        "Power Shard",
         "Imperial Seal",
         "Jade Tablet",
         "Tide Remnant",
-        "Gale Essence",
-        "Fusion Ring"
+        "Gale Essence"
     },
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/75c7fe88bf77410a404199a69629aae3.lua"))()
