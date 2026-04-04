fe77ca41f2cbf31e070a659aecef79c392ba11fa
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-05 00:51:40 +0700

    Update mammot.lua

--- mammot.lua
+++ mammot.lua
@@ -10,8 +10,8 @@ getgenv().sailorPieceConfig = {
     AUTO_FARM = { "Jinwoo Boss", "Alucard Boss", "Yuji Boss", "Gojo Boss", "Sukuna Boss", "Anos Boss", "Aizen Boss", "Strongest of Today Boss", "Strongest in History Boss", "Rimuru Boss", "Saber Boss", "QinShi Boss", "Ichigo Boss" },  
     SUMMON_BOSS = { "Ichigo", "Qin Shi", "Saber" },
 
-    EQUIP_SWORD = { "Ichigo", "Gryphon", "Dark Blade", "Katana" },
-    BLESS_WEAPON = { ["Ichigo"] = 10, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
+    EQUIP_WEAPON = { "Strongest in History", "Ichigo", "Gryphon", "Dark Blade", "Katana" },
+    BLESS_WEAPON = { ["Strongest in History"] = 10, ["Ichigo"] = 10, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
 
     STAT_POINT_PERCENTAGE = { ["Sword"] = 90, ["Defense"] = 10, ["Melee"] = 0, ["Power"] = 0 },
     USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
@@ -28,7 +28,7 @@ getgenv().sailorPieceConfig = {
     EQUIP_ARTIFACT_SET = "Celestial Rupture",
 
     -- Autotrade
-    TRADE_USERNAME = "FinnIsabellaYT789",
+    TRADE_USERNAME = { "FinnIsabellaYT789" },
     TRADE_ITEM = { "Race Reroll", "Clan Reroll", "Trait Reroll", "Haki Color Reroll", "Mythical Chest", "Secret Chest", "Aura Crate", "Cosmetic Crate", "Infinity Essence", "Blue Singularity", "Reversal Pulse", "Cursed Flesh", "Divine Grail", "Throne Remnant", "Ancient Shard", "Golden Essence", "Phantasm Core", "Broken Sword", "Azure Heart", "Silent Storm", "Yamato Essence", "Frozen Will", "Chrysalis Sigil", "Evolution Fragment", "Transcendent Core", "Fusion Ring", "Worthiness Fragment", "Hogyoku Frag", "Mirage Pendant", "Illusion Prism", "Reiatsu Core", "Boss Ticket", "Upper Seal", "Moon Crest", "Crescent Shard", "Lunar Essence", "Demon Remnant", "Abyss Sigil", "Atomic Core", "Atomic Omen", "Eminence Essence", "Shadow Remnant", "Magic Shard", "Void Seed", "Rush Key", "Umbral Capsule", "Dungeon Ticket", "Monarch Core", "Monarch Essence", "Kamish Dagger", "Shadow Crystal", "Abyss Edge", "Dark Ring", "Shadow Heart", "Dark Grail", "Corrupt Crown", "Corruption Core", "Alter Essence", "Morgan Remnant", "Cursed Finger", "Dismantle Fang", "Crimson Heart", "Battle Sigil", "Path Fragment", "Eternal Core", "Power Remnant", "Tempest Relic", "Sage Pulse", "Tempest Seal", "Slime Remnant", "Slime Core", "Calamity Seal", "Boss Rush Ticket", "Dungeon Key", "Tower Key", "Boss Key", "Passive Shard", "Power Shard", "Imperial Seal", "Jade Tablet", "Tide Remnant", "Gale Essence" },
 
     -- Discord
