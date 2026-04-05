b0180f9acbca20bf64da5499789cc65c583fca8b
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-05 08:35:37 +0700

    Update sailor-piece.lua

--- sailor-piece.lua
+++ sailor-piece.lua
@@ -1,2 +1,13 @@
 script_key="aDgJnOlAuVtVVAGGAjzULYXrBcFtQyHB";
+getgenv().SailorVIP = {
+    Optimization = {
+        Enable = false,
+        FPSCap = 10,
+    },
+
+    Webhook = {
+        Enable = true, 
+        URL = "https://discord.com/api/webhooks/1489367505226170430/3W21u4vaWuzWM4Mi7vf9e_jKm_XiTWh9x3GVGAFjqkIKQhErWNd4Ex_ohmoBfE0W1M7w", 
+    },
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
