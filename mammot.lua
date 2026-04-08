2ffdfcc78ddba0d2963cff0adb824e2ac007d96b
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-08 12:05:29 +0700

    Update mammot.lua

--- mammot.lua
+++ mammot.lua
@@ -28,7 +28,7 @@ getgenv().sailorPieceConfig = {
     EQUIP_ARTIFACT_SET = "Celestial Rupture",
 
     -- Autotrade
-    TRADE_USERNAME = { "FinnIsabellaYT789" },
+    TRADE_USERNAME = { "TestAccLana637289" },
     TRADE_ITEM = { "Race Reroll", "Clan Reroll", "Trait Reroll", "Haki Color Reroll", "Mythical Chest", "Secret Chest", "Aura Crate", "Cosmetic Crate", "Infinity Essence", "Divine Grail", "Throne Remnant", "Ancient Shard", "Golden Essence", "Phantasm Core", "Broken Sword", "Azure Heart", "Silent Storm", "Yamato Essence", "Frozen Will", "Chrysalis Sigil", "Evolution Fragment", "Transcendent Core", "Fusion Ring", "Worthiness Fragment", "Hogyoku Frag", "Mirage Pendant", "Illusion Prism", "Reiatsu Core", "Boss Ticket", "Upper Seal", "Moon Crest", "Crescent Shard", "Lunar Essence", "Demon Remnant", "Abyss Sigil", "Atomic Core", "Atomic Omen", "Eminence Essence", "Shadow Remnant", "Magic Shard", "Void Seed", "Rush Key", "Umbral Capsule", "Dungeon Ticket", "Monarch Core", "Monarch Essence", "Kamish Dagger", "Shadow Crystal", "Abyss Edge", "Dark Ring", "Shadow Heart", "Dark Grail", "Corrupt Crown", "Corruption Core", "Alter Essence", "Morgan Remnant", "Cursed Finger", "Dismantle Fang", "Crimson Heart", "Battle Sigil", "Path Fragment", "Eternal Core", "Power Remnant", "Tempest Relic", "Slime Core", "Calamity Seal", "Boss Rush Ticket", "Dungeon Key", "Tower Key", "Boss Key", "Passive Shard", "Power Shard", "Imperial Seal", "Jade Tablet", "Tide Remnant", "Gale Essence", "Frost Relic" },
 
     -- Discord
