e037338a2a4e90cf790ed92b859271111f07a6b6
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-18 16:09:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -39,4 +39,8 @@ fi
 # 3. Execution
 echo -e "${G}[+] BOOTING THE FOOL...${R}"
 echo -e "${GR}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${R}"
+
+# Reconnect standard input to the terminal to prevent EOFError during piped execution
+exec < /dev/tty
+
 python hopper.py
