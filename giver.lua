a59df022d8ae509061cb7cafcf059d222803601d
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-28 05:54:22 +0700

    Create giver.lua

---
+++ giver.lua
@@ -0,0 +1,3 @@
+script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
+getgenv().sailorPieceConfig = { KICK_IF_BOUNTY_UNDER = 500000 } 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/28db5da9f0424040df61ac39196b12d3.lua"))()
