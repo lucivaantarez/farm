af264ed4196d25b24792f6d1be3ef8e298cdd79b
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-27 09:28:44 +0700

    Update gift.lua

--- gift.lua
+++ gift.lua
@@ -4,7 +4,7 @@ setfpscap(10)
 getgenv().sailorPieceConfig = {  
     OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
     AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
-    KICK_IF_NO_TRADE_USERNAME = false,
+    KICK_IF_NO_TRADE_USERNAME = true,
     TRADE_SEA_1 = true,
     TRADE_USERNAME = { "k1myebo_m799", "Alka3825250", "pgik_jkjytti41", "Sw_bancroft", "lychee_blox6776" },  -- "Username"
     TRADE_ITEM = {        
