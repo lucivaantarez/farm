0f58556e557764e0dca6b696c21e05dcd6076553
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-28 05:55:25 +0700

    Create holder.lua

---
+++ holder.lua
@@ -0,0 +1,3 @@
+script_key="YrMGlHVZJYWpvtBaeKtZJOAtEBmcQbkQ";
+getgenv().sailorPieceConfig = { KICK_IF_BOUNTY_ABOVE = 50000000 }
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/27b4e2293c1c3e994ebbfd1dba6f862b.lua"))()
